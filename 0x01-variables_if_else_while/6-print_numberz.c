046ac1aa57590beb09e78e06cdaaa6fca1fddebb
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-06-05 17:59:53 +0200

    poow

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success/correct)
- *
- *     */
-
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
-
 {
+	int i;
 
-		int i;
-
-
-
-			for (i = 48; i < 58; i++)
-
-					{
-
-								putchar(i);
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
