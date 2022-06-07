04da31278d2ca317e186eb36885d2d43cf414f70
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-06-07 18:12:07 +0200

    pooo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	int i;
-	char marks[8]= {95, 112, 117, 116, 99, 104, 97, 114}; 
-	for(i=0;i<8;i++){
+	char marks[8] = {95, 112, 117, 116, 99, 104, 97, 114}; 
+	for(i=0; i< 8; i++)
+	{
          putchar(marks[i]) ;
 	}
 	printf("\n");
