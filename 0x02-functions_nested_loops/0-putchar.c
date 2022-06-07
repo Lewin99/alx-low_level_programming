b837282eef8aeb733b0f9edce3813fae764258e2
Lewin99 <l.ndahinyuk@alustudent.com>
2022-06-07 08:26:03 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,36 @@
+include <stdio.h>
+
+
+/*
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success/correct)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	    int i;
+
+	    	char marks[8]={95,112,117,116,99,104,97,114};
+
+			
+
+			for(i=0;i<8;i++){
+
+
+
+					putchar(marks[i]);
+
+						}
+
+				printf("\n");
+
+					return (0);
+
+
