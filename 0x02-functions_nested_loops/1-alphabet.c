3120ad2e95092eea1897f5b3c4ba7df7129e7279
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-06-07 19:30:31 +0200

    go

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - entry point
+ * Return: 0
+ */
+ void print_alphabet(void);
+   int main()
+   {
+    // Write C code here
+    print_alphabet();
+    printf("\n");
+    return 0;
+    }
+    
+     void print_alphabet()
+ {
+     for(char i=97; i<122; i++)
+     {
+         putchar(i);
+     }
+ }
