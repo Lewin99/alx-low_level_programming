7732ce10032e1197f206f75e3cbc0471ee25dee0
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-06-08 20:42:18 +0200

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -19,22 +18,3 @@ void print_alphabet(void)
 	_putchar('\n');
 
 }
-=======
-#include "main.h"
-/**
- * print_alphabet - entry point
- * Return: 0
- */
-void print_alphabet(void)
-{
-	char c = 'a';
-
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-
-	}
-	_putchar('\n');
-}
->>>>>>> 007b56a4f578d90336dc701d7992b7ce1b2abb00
