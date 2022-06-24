22b6446dd94d97d67ac72c18cb881afb61f7bc36
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-06-24 22:22:19 +0200

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,35 @@
-nclude "main.h"
+#include "main.h"
+
 /**
- *  * rot13 - encodes a string using rot13
- *   * @s: input string.
- *    * Return: the pointer to dest.
- *     */
-
-char *rot13(char *s)
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
 {
-	int count = 0, i;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+unsigned int m, d, count;
 
-	while (*(s + count) != '\0')
-	{
-
-		for (i = 0; i < 52; i++)
-		{
-			if (*(s + count) == alphabet[i])
-			{
-				*(s + count) = rot13[i];
-				break;
-			}
-		}
-		count++;
-	}
-
-	return (s);
+if (n < 0)
+{
+_putchar(45);
+m = n * -1;
+}
+else
+{
+m = n;
 }
 
+d = m;
+count = 1;
+
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+
+for (; count >= 1; count /= 10)
+{
+_putchar(((m / count) % 10) + 48);
+}
+}
