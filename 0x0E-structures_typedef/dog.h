8c38f91070ac5f55509c033c55f1a59d9e0ac994
Lewin99 <88319929+Lewin99@users.noreply.github.com>
2022-07-14 21:25:59 +0200

    bin

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,28 @@
-#include <stdio.h>
+#ifndef FILE_DOG
+#define FILE_DOG
 
+/**
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
+ */
 
-int main()
+struct dog
 {
-	struct dog
-	{
-	char* name;
+	char *name;
 	float age;
-	char* owner;
-	};
+	char *owner;
+};
 
-	return 0;
-}
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
